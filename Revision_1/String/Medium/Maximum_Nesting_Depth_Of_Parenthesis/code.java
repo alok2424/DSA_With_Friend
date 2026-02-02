f45d6320ddc3ec6aab141145e914291886b37a5e
alok2424 <22ec059@jssaten.ac.in>
2026-02-02 12:49:09 +0530

    "max-nesting-depth"

---
+++ Revision_1/String/Medium/Maximum_Nesting_Depth_Of_Parenthesis/code.java
@@ -0,0 +1,16 @@
+class Solution{
+    // Method to compute maximum depth of parentheses
+    public int maxDepth(String s) {
+        int p = 0; 
+        int ans = 0; 
+        for (char ch : s.toCharArray()) {
+            // Increase depth on open parenthesis
+            if (ch == '(') p++;
+            // Decrease depth on close parenthesis
+            else if (ch == ')') p--;
+            // Update maximum depth encountered
+            ans = Math.max(ans, p);
+        }
+        return ans; 
+    }
+}
